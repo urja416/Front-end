71d6a816747c09f1691e1322413264c3f5463166
urja416 <urjadhungana008@gmail.com>
2024-03-19 17:46:28 +0545

    ch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound'
+import RootLayOut from './components/RootLayOut'
+import Category from './components/Category'
+import SearchPage from './pages/SearchPage'
+import DetailPage from './pages/DetailPage'
+import MoviePage from './pages/MoviePage'
+const App = () => {
 
-function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Routes>
+        <Route path='/' element={<RootLayOut />} >
+          <Route index element={<HomePage />} />
+          <Route path='movieCategory/:category' element={<Category />} />
+          <Route path='moviePage/:category/:page' element={<MoviePage />} />
+          <Route path='movieSearch/:search' element={<SearchPage />} />
+          <Route path='movieDetail/:id' element={<DetailPage />} />
+          <Route path='*' element={<NotFound />} />
+
+        </Route>
+
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
