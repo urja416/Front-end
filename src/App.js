941f3c5796f4488231177ad6a3ed9fa7756aece1
urja416 <urjadhungana008@gmail.com>
2024-04-07 21:07:20 +0545

    ch

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-
-import RootLayOut from "./ui/RootLayOut";
-import HomePage from "./Features/home/HomePage";
-import Login from "./Features/auth/Login";
-import SignUp from "./Features/auth/SignUp";
+import React from 'react'
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: < RootLayOut />,
-      children: [
-        { index: true, element: <HomePage /> },
-        { path: 'login', element: <Login /> },
-        { path: 'signup', element: <SignUp /> },
-        { path: 'about', element: <h1>jshdfhgdb </h1> }
+  return (
+    <div>
 
-      ]
-    },
-
-  ]);
-
-
-  return <RouterProvider router={router} />
-
-
+    </div>
+  )
 }
 
 export default App
+
